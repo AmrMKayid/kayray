4d5497118d3ba3fffb40acf0e609048538e7aeee
AmrMKayid <amrkayid2027@gmail.com>
2019-07-11 15:01:18 +0200

    Modify experiments stop conditions

--- kayray/envs/unity.py
+++ kayray/envs/unity.py
@@ -1,9 +1,9 @@
-# from mlagents.envs import UnityEnvironment
-from gym_unity.envs import UnityEnv
-from kayray.utils import glogger, describe
-
-def make_unity_env(env_name):
-	env_name = f'build/{env_name}'  # Name of the Unity environment binary to launch
-	env = UnityEnv(env_name, worker_id=0, use_visual=True)
-	# env = UnityEnvironment(file_name=env_name, worker_id=0)
-	glogger.info(describe(env))
+# # from mlagents.envs import UnityEnvironment
+# from gym_unity.envs import UnityEnv
+# from kayray.utils import glogger, describe
+#
+# def make_unity_env(env_name):
+# 	env_name = f'build/{env_name}'  # Name of the Unity environment binary to launch
+# 	env = UnityEnv(env_name, worker_id=0, use_visual=True)
+# 	# env = UnityEnvironment(file_name=env_name, worker_id=0)
+# 	glogger.info(describe(env))
