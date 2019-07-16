7e77eff6b65150750d6f5f9a62bd81c8e46c48a2
AmrMKayid <amrkayid2027@gmail.com>
2019-07-16 22:55:36 +0200

    Create MultiAgent Unity env

--- kayray/envs/unity.py
+++ kayray/envs/unity.py
@@ -6,9 +6,47 @@ from gym_unity.envs import UnityEnv
 from kayray.utils import glogger, describe
 from collections import OrderedDict
 
+from ray.rllib.env.multi_agent_env import MultiAgentEnv
+
+
 COUNTER = 0
 PATH = os.path.dirname(os.path.abspath(__file__))
 
+class MultiAgentsUnityRayEnv(MultiAgentEnv):
+    def __init__(self, env_config, env_name, no_graphics=True, multiagent=True, use_visual=False):
+        glogger.info(f'Starting env: {env_name} | worker_id: {env_config.worker_index}')
+        env_name = f'{PATH}/build/{env_name}'
+        self._env = UnityEnv(environment_filename=env_name, worker_id=env_config.worker_index, no_graphics=no_graphics, multiagent=multiagent)
+        self.agents = self._env._n_agents
+        self.dones = set()
+        self.observation_space = self._env.observation_space
+        self.action_space = self._env.action_space
+        self.resetted = False
+        glogger.info(describe(self._env))
+        glogger.info(describe(self))
+        
+    def reset(self):
+        self.resetted = True
+        self.dones = set()
+        last_obs = self._env.reset()
+        return {i: obs for i, obs in enumerate(last_obs)}
+        
+    def step(self, action_dict):
+        actions = list(action_dict.values())
+        # glogger.fatal(actions)
+        obs, rew, done, info = {}, {}, {}, {}
+        obs_list, rew_list, done_list, info_list = self._env.step(actions)
+        for i in range(self.agents):
+            obs[i], rew[i], done[i], info[i] = obs_list[i], rew_list[i], done_list[i], info_list
+            if done[i]:
+                self.dones.add(i)
+        done["__all__"] = len(self.dones) == self.agents
+        
+        # glogger.fatal(obs, rew, done, info)
+        return obs, rew, done, info
+        
+    
+    
 class UnityRayEnv(gym.Env):
     def __init__(self, env_config, env_name, no_graphics=True, multiagent=True, use_visual=False):
         glogger.info(f'Starting env: {env_name} | worker_id: {env_config.worker_index}')
@@ -27,12 +65,15 @@ class UnityRayEnv(gym.Env):
             actions = list(actions.values())
         # glogger.fatal(f'Actions: {actions}')
         obs, rewards, dones, infos = self._env.step(actions)
-        # glogger.fatal(obs, rewards, dones, infos)
+        glogger.fatal(obs, rewards, dones, infos)
         obs = spaces.Dict({f'obs_{i}': o for i, o in enumerate(obs)})
         return obs, rewards, dones, infos
 
 def make_unity_env(env_config, env_name, worker_id=0,  no_graphics=True):
-	return UnityRayEnv(env_config, env_name)
+    multiagent = env_name[-2:] == '20'
+    if multiagent:
+        return MultiAgentsUnityRayEnv(env_config, env_name)
+    return UnityRayEnv(env_config, env_name)
 
 # def make_unity_env(env_config, env_name, worker_id=0,  no_graphics=True):
 # 	# return UnityRayEnv(env_config, env_name)
--- test_env2.py
+++ test_env2.py
@@ -1,9 +1,54 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+
+import ray
+from ray.tune import run_experiments, register_env
+from ray.rllib.models import ModelCatalog
+
 from kayray import DEFAULT_RESULTS_DIR
 from kayray.envs import make_env, make_unity_env
 from kayray.utils import set_global_seeds, make_parser, DotDict, glogger
 
-env = make_unity_env(DotDict({'worker_index': 0}), 'Reacher20')
-env.reset()
-obs, rew, dones, info = env.step(env.action_space.sample())
+# env = make_unity_env(DotDict({'worker_index': 0}), 'Reacher20', no_graphics=False)
+# obs = env.reset()
+# # print(obs)
+# rews = []
+# for i in range(1000):
+#     actions = {}
+#     for a in range(env.agents):
+#         actions.update({f'Action_{a}': env.action_space.sample()})
+#     # glogger.info(actions)
+#     obs, rew, dones, info = env.step(actions)
+#     rews.append(rew)
+# glogger.info(rews)
 
 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num-iters", type=int, default=100)
+    parser.add_argument("--num-workers", type=int, default=0)
+    args = parser.parse_args()
+
+    ray.init()
+
+    env_name = 'Reacher20'
+    register_env("unityml", lambda env_config: make_unity_env(env_config=env_config, env_name=env_name))
+
+    run_experiments({
+        "ppo_sc2": {
+            "run": "PPO",
+            "env": "unityml",
+            "stop": {
+                "training_iteration": args.num_iters,
+            },
+            "config": {
+                "num_workers": args.num_workers,
+                "observation_filter": "NoFilter",  # breaks the action mask
+                "vf_share_layers": True,  # don't create a separate value model
+            },
+        },
+     })
